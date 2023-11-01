4d9e23610b34e7bb91ec5594887169800391cbb8
Maddie Weber <maddieweber7@gmail.com>
2023-11-01 11:52:33 -0400

    done

--- index.js
+++ index.js
@@ -11,6 +11,54 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+// const titleCased = tutorials.map(() => {
+
+//   return tutorials
+// });
+
+// Your job is to write the following function:
+
+// titleCased(): returns an array with title case tutorial names. Note that this function takes no arguments and should use the global 
+//tutorials variable as data.
+// NOTE: This lab is challenging! You will need to iterate through the tutorials array, modifying the name of each tutorial.
+// To do this, you will also need to access and modify each individual word.
+
+//STEPS
+// use a for each loop to grab each string inside tutorials
+// split strings into sub-strings by dividing where there is a space
+// grab the first letter of each word and capitalize it
+// recombine substrings
+
+
+// function titleCased() {
+
+//   // In this line, a new constant titleCasedTutorials is defined to store the result of the map() 
+//   //function applied to the tutorials array. The map() function takes a callback function that will
+//   // be applied to each element in the tutorials array.
+//   const titleCasedTutorials = tutorials.map(function (tutorial) {
+//     const words = tutorial.split(' '); // Split the string into words
+//     for (let i = 0; i < words.length; i++) {
+
+//       //this code capitalizes the first letter of each word. It takes the i-th word, extracts the first 
+//       //character using charAt(0), capitalizes it using toUpperCase(), and then appends the rest of the word 
+//       //using slice(1).
+//       words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1); // Capitalize the first letter
+//     }
+//     return words.join(' '); // Join the words back together and update the array
+//   });
+//   return titleCasedTutorials
+// }
+
+function titleCased() {
+  const titleCasedTutorials = tutorials.map(function(tutorial) {
+    const words = tutorial.split(' ');
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+  });
+  return titleCasedTutorials
+};
+
+
+console.log(titleCased(tutorials));
